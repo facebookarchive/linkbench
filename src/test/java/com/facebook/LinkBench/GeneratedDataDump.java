a07281289bb8cd4ddd72e12c3998ebdd71b418ec
Tim Armstrong <tarmstrong@fb.com>
2012-11-09 14:22:24 -0600

    Add in payload generator driver to test compressibility
    Summary:
    Tweak object motif to get ~60% compressibility and assoc to get ~30%
    
    This driver was used to discover parameters for data generators that had similar compressibility to real data.
    
    Test Plan: Ran and inspected output
    
    Reviewers: dhruba
    
    Reviewed By: dhruba
    
    Differential Revision: https://reviews.facebook.net/D6621

---
+++ src/test/java/com/facebook/LinkBench/GeneratedDataDump.java
@@ -0,0 +1,103 @@
+package com.facebook.LinkBench;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
+
+import com.facebook.LinkBench.generators.DataGenerator;
+import com.facebook.LinkBench.generators.MotifDataGenerator;
+import com.facebook.LinkBench.generators.UniformDataGenerator;
+/**
+ * Generate some sample data using data generators, in order to test out compressibility
+ * of randomly generated data.
+ * 
+ * This generates several output files consistent of many generated payload data fields
+ * separated by newlines
+ */
+public class GeneratedDataDump {
+  private static final Random rng = new Random();
+  
+  public static void main(String args[]) {
+    String outputDir = "";
+    if (args.length == 0) {
+      outputDir = ".";
+    } else if (args.length == 1) {
+      outputDir = args[0];
+    } else {
+      System.err.println("GeneratedDataDump <output dir>");
+      System.exit(1);
+    }
+    
+    
+    // Number of bytes per row
+    final int objBytes = 256;
+    final int assocBytes = 6;
+    // Number of rows to generate
+    final int objRows = 250000;
+    final int assocRows = 10000000;
+    writeGeneratedDataFile(outputDir + "/gen-data-motif.txt", makeMotifObj(), objRows, objBytes);
+    writeGeneratedDataFile(outputDir + "/gen-data-uniform.txt", makeUniformObj(), objRows, objBytes);
+    writeGeneratedDataFile(outputDir + "/gen-data-assoc-motif.txt", makeMotifAssoc(), assocRows, assocBytes);
+    writeGeneratedDataFile(outputDir + "/gen-data-assoc-uniform.txt", makeUniformAssoc(), assocRows, assocBytes);
+  }
+
+  private static void writeGeneratedDataFile(String outFileName,
+      DataGenerator gen, int rows, int bytes) {
+    OutputStream out = null;
+    try {
+      out = new BufferedOutputStream(new FileOutputStream(outFileName));
+    } catch (FileNotFoundException e) {
+      System.err.println("file " + outFileName + " could not be opened");
+      System.exit(1);
+    }
+    
+    
+    byte buf[] = new byte[bytes];
+    
+    try {
+      for (int i = 0; i < rows; i++) {
+        gen.fill(rng, buf);
+        out.write(buf);
+        out.write('\n');
+      }
+      out.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  private static DataGenerator makeUniformObj() {
+    UniformDataGenerator gen = new UniformDataGenerator();
+    gen.init(50, 75);
+    return gen;
+  }
+
+  private static MotifDataGenerator makeMotifObj() {
+    MotifDataGenerator gen = new MotifDataGenerator();
+    int start = 50;
+    int end = 220;
+    double uniqueness = 0.63;
+    gen.init(start, end, uniqueness);
+    return gen;
+  }
+  
+  private static DataGenerator makeUniformAssoc() {
+    UniformDataGenerator gen = new UniformDataGenerator();
+    gen.init(50, 75);
+    return gen;
+  }
+
+  private static MotifDataGenerator makeMotifAssoc() {
+    MotifDataGenerator gen = new MotifDataGenerator();
+    int start = 32;
+    int end = 100;
+    double uniqueness = 0.225;
+    int motifSize = 128;
+    gen.init(start, end, uniqueness, motifSize);
+    return gen;
+  }
+}
